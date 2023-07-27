164d71f5616a86a83bc92885a03a392b0991b32d
hahahahacker2009 <hahahahacker2009@gmail.com>
2023-07-27 17:25:46 +0700

    anonymous access configuration

---
+++ extra/config_access.php
@@ -0,0 +1,31 @@
+<?php
+
+// --------- User config ----------------------------------------------
+
+$anonymous_access = true;
+$anonymous_page = '';
+
+
+// --------- Scripts --------------------------------------------------
+
+$skip_scripts = array(
+	'/account/index.php',
+	'/account/login.php',
+	'/account/register.php'
+);
+
+if (!empty($anonymous_page)) {
+	$skip_scripts[] = $anonymous_page;
+}
+
+if (!in_array($_SERVER['SCRIPT_NAME'], $skip_scripts)) {
+	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
+	require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/security.php");
+	$auth = NULL;
+	if (isset($_SESSION['auth'])) {
+		$auth = $_SESSION['auth'];
+	}
+	security_validateupdateinfo($dbc, $auth);
+	security_authlastvisit($dbc, $auth, $_SERVER['REMOTE_ADDR']);
+}
+?>
