4eaf55c3018c8e66cfb8ffefdbc1384bd21f217c
hahahahacker2009 <hahahahacker2009@gmail.com>
2023-07-27 17:26:59 +0700

    access configuration in a separate file

--- extra/config.php
+++ extra/config.php
@@ -8,18 +8,6 @@ $server = $_SERVER['HTTP_HOST'];
 
 
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/variables.php");
-
-$skip_scripts = array(
-	'/account/index.php',
-	'/account/login.php',
-	'/account/register.php'
-);
-if (!in_array($_SERVER['SCRIPT_NAME'], $skip_scripts)) {
-	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
-	require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/security.php");
-	$auth = $_SESSION['auth'];
-	security_validateupdateinfo($dbc, $auth);
-	security_authlastvisit($dbc, $auth, $_SERVER['REMOTE_ADDR']);
-}
+require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config_access.php");
 
 ?>
