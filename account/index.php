188f43036a0d544eae0c8afd00aeeb0871cfd340
hahahahacker2009 <hahahahacker2009@gmail.com>
2023-08-04 17:18:05 +0700

    print user information from session

--- account/index.php
+++ account/index.php
@@ -5,16 +5,9 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>
 
 <?php
-	if (isset($_SESSION['auth'])) {
-		require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
-		$auth = $_SESSION['auth'];
-		$query = "SELECT name FROM forum_user WHERE password='$auth'";
-		$result = @mysqli_query($dbc, $query);
-		if ($result) {
-			$assoc = mysqli_fetch_assoc($result);
-			echo "<p>Chao mung, {$assoc['name']}!</p>";
-			echo "<p><a href=\"$protocol://$server/account/logout.php\">Dang xuat?</a></p>";
-		}
+	if ($_SESSION['ready'] === TRUE) {
+		echo "<p>Chao mung, {$_SESSION['name']}!</p>";
+		echo "<p><a href=\"$protocol://$server/account/logout.php\">Dang xuat?</a></p>";
 	} else {
 		echo "<p>Ban chua dang nhap.</p>";
 		echo "<p><a href=\"$protocol://$server/account/login.php\">Dang nhap tai khoan</a> ";
