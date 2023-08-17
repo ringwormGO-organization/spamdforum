010c4ebda05e08765f56f9bb9ac116df252a4951
Someone <hahahahacker2009@gmail.com>
2023-08-17 17:52:14 +0700

    specify all attributes, use secure connection if possible, specify samesite to lax

--- extra/config.php
+++ extra/config.php
@@ -1,9 +1,14 @@
 <?php
-session_start();
 $sitename = 'spamdforum';
 $protocol = 'http';
 $server = $_SERVER['HTTP_HOST'];
 //$server = 'hostname.com';
+$secure = false;
+if ($protocol == 'https' && $_SERVER['HTTPS']) {
+	$secure = true;
+}
+session_set_cookie_params(['lifetime' => time() + 2592000, 'path' => '/', 'domain' => '', 'secure' => $secure, 'httponly' => true, 'samesite' => 'Lax']);
+session_start();
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/variables.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config_access.php");
 
