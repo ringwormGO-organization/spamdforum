606e0d77a2dc0cdde8d92f04a9201a557f6e42d7
Someone <hahahahacker2009@gmail.com>
2023-08-17 14:38:58 +0700

    using prepared statement

--- profiles.php
+++ profiles.php
@@ -9,14 +9,11 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 
 <?php
 if (!empty($_SERVER['PATH_INFO']) || isset($_GET['email'])) {
-	//$email = explode("/", $_SERVER['PATH_INFO']); // not working because /musicscratchgame2009@gmail.com
-	// will implement both /emailaddr and ?email=emailaddr, but prefer the first one
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
 	$email = escape_data($_GET['email']);
 	//$query = "SELECT user_id, name, powerlevel, reg_date, last_visit FROM forum_user WHERE email='$email[0]'";
-	$query = "SELECT user_id, name, powerlevel, reg_date, last_visit FROM forum_user WHERE email='$email'";
-	echo "<pre>Cau truy van se duoc thuc hien: $query</pre>";
-	$result = mysqli_query($dbc, $query);
+	$query = "SELECT user_id, name, powerlevel, reg_date, last_visit FROM forum_user WHERE email=?";
+	$result = mysqli_execute_query($dbc, $query, [$email]);
 	if (mysqli_num_rows($result) == 1) {
 		$userdatarow = mysqli_fetch_row($result);
 		echo "<h2><a href=\"$protocol://$server/profiles.php?email=$email\">$userdatarow[1] ($userdatarow[2])</h2><p><a href=\"mailto:$email\">$email</a></p>";
