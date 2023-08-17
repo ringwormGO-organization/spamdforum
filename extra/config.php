3466de1b210f70ae578051d052a2aec129e287cb
Someone <hahahahacker2009@gmail.com>
2023-08-17 17:12:34 +0700

    remove trash data

--- extra/config.php
+++ extra/config.php
@@ -2,11 +2,8 @@
 session_start();
 $sitename = 'spamdforum';
 $protocol = 'http';
-//$server = 'npg2mvrzxadbsv5x6gc3a7ahhrveluyurdpirwfc7mhhpfrvbnvbrdyd.onion';
-//$server = '0.0.0.0:8000';
 $server = $_SERVER['HTTP_HOST'];
-
-
+//$server = 'hostname.com';
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/variables.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config_access.php");
 
