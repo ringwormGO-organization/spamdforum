f9c155d77f13574228bb51f1d4832cf0393f09d6
Someone <hahahahacker2009@gmail.com>
2023-08-19 10:43:15 +0700

    for banned user

--- profiles.php
+++ profiles.php
@@ -3,7 +3,6 @@ require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config.php");
 ?>
 
 <?php
-$title = "Ho so nguoi dung";
 include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>
 
@@ -11,18 +10,22 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 if (!empty($_GET['email'])) {
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
 	$email = escape_data($_GET['email']);
-	$query = "SELECT user_id, name, powerlevel, reg_date, last_visit FROM forum_user WHERE email=?";
+	$query = "SELECT user_id, name, password powerlevel, reg_date, last_visit FROM forum_user WHERE email=?";
 	$result = mysqli_execute_query($dbc, $query, [$email]);
 	if (mysqli_num_rows($result) == 1) {
 		$userdata = mysqli_fetch_row($result);
 		foreach ($userdata as $key => $value) {
 			$userdata[$key] = export_data($value);
 		}
-		echo "<h2><a href=\"$protocol://$server/profiles.php?email=$email\">$userdata[1] ($userdata[2])</h2><p><a href=\"mailto:$email\">$email</a></p>";
-		echo "<h3>Ngay dang ky: {$userdata[3]}</h3>";
-		echo "<p>Truy cap lan cuoi: {$userdata[4]}</p>";
+		if (!empty($userdata[2])) {
+			echo "<h2><a href=\"$protocol://$server/profiles.php?email=$email\">$userdata[1] ($userdata[3])</a> <a href=\"mailto:$email\">&lt;$email&gt</a></h2>";
+		} else {
+			echo "<h2 id=\"banneduser\"><a href=\"$protocol://$server/profiles.php?email=$email\">$userdata[1] ($userdata[3])</a> <a href=\"mailto:$email\">&lt;$email&gt</a></h2>";
+		}
+		echo "<h3>{$profilesphp['reg_date']}: {$userdata[3]}</h3>";
+		echo "<p>{$profilesphp['last_visit']}: {$userdata[4]}</p>";
 	} else {
-		echo "<h3>Khong tim thay nguoi dung!</h3>";
+		echo "<h3>{$profilesphp['err_not_found']}</h3>";
 	}
 }
 ?>
