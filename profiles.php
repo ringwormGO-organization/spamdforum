e658fcdeab0e21f3935bcf489b9b566c40ed8ade
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-02-29 22:39:59 +0700

    rename userdata to uinfo, use del tag instead of css, more newline

--- profiles.php
+++ profiles.php
@@ -18,17 +18,22 @@ if (!empty($_GET['email'])) {
 		reg_date, last_visit FROM forum_user WHERE email=?";
 	$result = mysqli_execute_query($dbc, $query, [$email]);
 	if (mysqli_num_rows($result) == 1) {
-		$userdata = mysqli_fetch_row($result);
-		foreach ($userdata as $key => $value) {
-			$userdata[$key] = export_data($value);
+		$uinfo = mysqli_fetch_row($result);
+		foreach ($uinfo as $key => $value) {
+			$uinfo[$key] = export_data($value);
 		}
-		if (!empty($userdata[2])) {
-			echo "<h2><a href=\"$protocol://$server/profiles.php?email=$email\">$userdata[1] ($userdata[3])</a> <a href=\"mailto:$email\">&lt;$email&gt</a></h2>";
+		if (!empty($uinfo[2])) {
+			echo "<h2><a href=\"$protocol://$server/profiles.php" .
+			    "?email=$email\">$uinfo[1]</a> ($uinfo[3]) " .
+			    "<a href=\"mailto:$email\">&lt;$email&gt;</a></h2>";
 		} else {
-			echo "<h2 id=\"banneduser\"><a href=\"$protocol://$server/profiles.php?email=$email\">$userdata[1] ($userdata[3])</a> <a href=\"mailto:$email\">&lt;$email&gt</a></h2>";
+			echo "<h2><del><a href=\"$protocol://$server/" .
+			    "profiles.php?email=$email\">$uinfo[1]</a>" .
+			    "</del> ($uinfo[3]) <a href=\"mailto:$email\">" .
+			    "&lt;$email&gt;</a></h2>";
 		}
-		echo "<h3>{$profilesphp['reg_date']}: {$userdata[4]}</h3>";
-		echo "<p>{$profilesphp['last_visit']}: {$userdata[5]}</p>";
+		echo "<h3>{$profilesphp['reg_date']}: {$uinfo[4]}</h3>";
+		echo "<p>{$profilesphp['last_visit']}: {$uinfo[5]}</p>";
 	} else {
 		echo "<h3>{$profilesphp['err_not_found']}</h3>";
 	}
