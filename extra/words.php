7cd91cb6a0e4008cf5436bc94427b0ceb4417f6a
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-02-29 22:49:50 +0700

    remove a newline

--- extra/words.php
+++ extra/words.php
@@ -1,7 +1,6 @@
 <?php
 /* See file LICENSE for permissions and conditions to use the file. */
 ?>
-
 <?php
 require("{$_SERVER['DOCUMENT_ROOT']}/extra/words/words_vi.php");
 //require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/words/words_en.php");
