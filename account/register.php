5fb4ed01d75a2c07c6217e9ca6b570eeaa95d44f
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-06-11 11:57:27 +0700

    better code, remove label tags

--- account/register.php
+++ account/register.php
@@ -1,7 +1,6 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
 ?>
-
 <?php
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config.php");
 ?>
@@ -32,65 +31,58 @@ function valid_passwd($passwd)
 }
 ?>
 <?php
-	if (isset($_SESSION['auth'])) {
+if (isset($_SESSION['auth'])) {
+	header("Location: $protocol://$server/index.php");
+	exit();
+}
+if (!$registration_status) {
+	goto html;
+}
+
+if (isset($_POST['register'])) {
+	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
+	include_once("{$_SERVER['DOCUMENT_ROOT']}/extra/words.php");
+	$msg = NULL;
+	$name = $email = $password = false;
+
+	if (!($name = valid_name($_POST['name']))) {
+		$msg .= $registerphp['msg']['err_name'];
+	}
+
+	if (!($email = valid_email($_POST['email']))) {
+		$msg .= $registerphp['msg']['err_email'];
+	}
+
+	if ($_POST['password'] == $_POST['verify']) {
+		if (!($passwd = valid_passwd($_POST['password']))) {
+			$msg .= $registerphp['msg']['err_password'];
+		}
+	} else {
+		$msg .= $registerphp['msg']['err_password_mismatch'];
+	}
+
+	if (! ($name && $email && $passwd)) {
+		$msg .= $registerphp['msg']['err_tryagain'];
+		goto html;
+	}
+	if (check_exist_email($email, $dbc)) {
+		$msg .= $registerphp['msg']['err_email_existed'];
+		goto html;
+	}
+	$password = secure_hash($passwd, PASSWORD_BCRYPT);
+	$query = "INSERT INTO $table (name, email, password, powerlevel, reg_date, last_visit, last_ip) VALUES (?, ?, ?, ?, NOW(), NOW(), ?);";
+	if (mysqli_execute_query($dbc, $query, [$name, $email, $password, 0, $_SERVER['REMOTE_ADDR']])) {
+		$_SESSION['auth'] = $password;
 		header("Location: $protocol://$server/index.php");
 		exit();
+	} else {
+		$msg .= $registerphp['msg']['err_server'] . mysqli_error($dbc);
 	}
-	if (!$registration_status) {
-		include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
-		echo "<h1>{$registerphp['reg_disabled']}</h1>";
-		include("{$_SERVER['DOCUMENT_ROOT']}/html/footer.html");
-		exit();
-	}
-
-	if (isset($_POST['register'])) {
-		require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
-		include_once("{$_SERVER['DOCUMENT_ROOT']}/extra/words.php");
-		$msg = NULL;
-		$name = $email = $password = false;
-
-		if (!($name = valid_name($_POST['name']))) {
-			$msg .= $registerphp['msg']['err_name'];
-		}
-
-		if (!($email = valid_email($_POST['email']))) {
-			$msg .= $registerphp['msg']['err_email'];
-		}
-
-		if ($_POST['password'] == $_POST['verify']) {
-			if (!($passwd = valid_passwd($_POST['password']))) {
-				$msg .= $registerphp['msg']['err_password'];
-			}
-		} else {
-			$msg .= $registerphp['msg']['err_password_mismatch'];
-		}
-
-		if ($name && $email && $passwd) {
-			if (check_exist_email($email, $dbc)) {
-				include "{$_SERVER['DOCUMENT_ROOT']}/html/header.html";
-				$msg .=
-				$registerphp['msg']['err_email_existed'];
-				include "{$_SERVER['DOCUMENT_ROOT']}/html/footer.html";
-				exit;
-			}
-			$password = secure_hash($passwd, PASSWORD_BCRYPT);
-			$query = "INSERT INTO $table (name, email, password, powerlevel, reg_date, last_visit, last_ip) VALUES (?, ?, ?, ?, NOW(), NOW(), ?);";
-			if (mysqli_execute_query($dbc, $query, [$name, $email, $password, 0, $_SERVER['REMOTE_ADDR']])) {
-				$_SESSION['auth'] = $password;
-				header("Location: $protocol://$server/index.php");
-				exit();
-			} else {
-				$msg .= $registerphp['msg']['err_server'] . mysqli_error($dbc);
-			}
-			mysqli_close($dbc);
-
-		} else {
-			$msg .= $registerphp['msg']['err_tryagain'];
-		}
-	}
+	mysqli_close($dbc);
+}
 ?>
-
 <?php                                 
+	html:
 	include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>                                                                      
 <h1><?=$registerphp['h1_title']; ?></h1>
@@ -107,25 +99,25 @@ if (isset($msg)) {
 <legend><b>Nhap vao thong tin cua ban</b></legend>
 <table style="text-align: center; margin-left:auto; margin-right:auto; 
               border-width:0; width:100%;">
-	<tr>
-		<th style="width:30%;"><?php echo $registerphp['form_input']['name']; ?>:</th> 
-		<td><label for="name"><input id="name" type="text" name="name" size="64" maxlength="127" value="<?php if(isset($_POST['name'])) {echo export_data($_POST['name']);} ?>"></label></td>
-	</tr>
-	<tr>	
-		<th style="width:30%;"><?php echo $registerphp['form_input']['email']; ?>:</th> 
-		<td><label for="email"><input id="email" type="text" name="email" size="64" maxlength="127" value="<?php if(isset($_POST['email'])) {echo export_data($_POST['email']);} ?>"></label></td>
-	</tr>
-	<tr>
-		<th style="width:30%;"><?php echo $registerphp['form_input']['password']; ?>:</th> 
-		<td><label for="password"><input id="password" type="password" name="password" size="64" maxlength="64"></label></td>
-	</tr>
-	<tr>
-		<th style="width:30%;"><?php echo $registerphp['form_input']['verify']; ?>:</th> 
-		<td><label for="verify"><input id="verify" type="password" name="verify" size="64" maxlength="64"></label></td>
-	</tr>
-	<tr>
-		<td style="width:30%;"><label for="register"><input id="register" type="submit" name="register" value="<?php echo $registerphp['form_input']['register']; ?>!"></label></td>
-	</tr>
+<tr>
+	<th style="width:30%;"><?php echo $registerphp['form_input']['name']; ?>:</th> 
+	<td><input id="name" type="text" name="name" size="64" maxlength="127" value="<?php if(isset($_POST['name'])) {echo export_data($_POST['name']);} ?>"></td>
+</tr>
+<tr>	
+	<th style="width:30%;"><?php echo $registerphp['form_input']['email']; ?>:</th> 
+	<td><input id="email" type="text" name="email" size="64" maxlength="127" value="<?php if(isset($_POST['email'])) {echo export_data($_POST['email']);} ?>"></td>
+</tr>
+<tr>
+	<th style="width:30%;"><?php echo $registerphp['form_input']['password']; ?>:</th> 
+	<td><input id="password" type="password" name="password" size="64" maxlength="64"></td>
+</tr>
+<tr>
+	<th style="width:30%;"><?php echo $registerphp['form_input']['verify']; ?>:</th> 
+	<td><input id="verify" type="password" name="verify" size="64" maxlength="64"></td>
+</tr>
+<tr>
+	<td style="width:30%;"><label for="register"><input id="register" type="submit" name="register" value="<?php echo $registerphp['form_input']['register']; ?>!"></label></td>
+</tr>
 </table>
 </fieldset>
 
