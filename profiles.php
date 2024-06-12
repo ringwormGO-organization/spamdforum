e62f9b052fd554d7855e8c190b9c0723bf2a49d2
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-06-12 16:33:47 +0700

    don't look if the password is empty or not to decide if the user is banned

--- profiles.php
+++ profiles.php
@@ -11,7 +11,7 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 if (!empty($_GET['email'])) {
 	require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
 	$email = escape_data($_GET['email']);
-	$query = "SELECT user_id, name, password, powerlevel, 
+	$query = "SELECT user_id, name, powerlevel,
 		reg_date, last_visit FROM forum_user WHERE email=?";
 	$result = mysqli_execute_query($dbc, $query, [$email]);
 	if (mysqli_num_rows($result) < 1) {
@@ -22,18 +22,18 @@ if (!empty($_GET['email'])) {
 	foreach ($uinfo as $key => $value) {
 		$uinfo[$key] = export_data($value);
 	}
-	if (!empty($uinfo[2])) {
+	if ($uinfo[2] >= 0) {
 		echo "<h2><a href=\"$protocol://$server/profiles.php" .
-		    "?email=$email\">$uinfo[1]</a> ($uinfo[3]) " .
+		    "?email=$email\">$uinfo[1]</a> ($uinfo[2]) " .
 		    "<a href=\"mailto:$email\">&lt;$email&gt;</a></h2>";
 	} else {
 		echo "<h2><del><a href=\"$protocol://$server/" .
 		    "profiles.php?email=$email\">$uinfo[1]</a>" .
-		    "</del> ($uinfo[3]) <a href=\"mailto:$email\">" .
+		    "</del> ($uinfo[2]) <a href=\"mailto:$email\">" .
 		    "&lt;$email&gt;</a></h2>";
 	}
-	echo "<h3>{$profilesphp['reg_date']}: {$uinfo[4]}</h3>";
-	echo "<p>{$profilesphp['last_visit']}: {$uinfo[5]}</p>";
+	echo "<h3>{$profilesphp['reg_date']}: {$uinfo[3]}</h3>";
+	echo "<p>{$profilesphp['last_visit']}: {$uinfo[4]}</p>";
 }
 ?>
 <?php
