ee5241f975e2a8ed32d8e686d8115f72adc42147
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-06-12 16:24:03 +0700

    remove blank line

--- index.php
+++ index.php
@@ -1,12 +1,10 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
 ?>
-
 <?php
 $title = "Trang chu";
 include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>
-
 <?php
 include("{$_SERVER['DOCUMENT_ROOT']}/html/footer.html");
 ?>
