8979cfad237c7e290b2555e3ebd9038d13254335
hahahahacker2009 <hahahahacker2009@gmail.com>
2024-06-13 18:38:36 +0700

    comments, related message link

--- view.php
+++ view.php
@@ -10,7 +10,13 @@ if (empty($_GET['id'])) {
 	exit;
 }
 $id = $_GET['id'];
-$assoc = mysqli_fetch_assoc(mysqli_execute_query($dbc, "SELECT * FROM $msgtable WHERE msg_id=?", [$id]));
+$result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable WHERE msg_id=?", [$id]);
+if (mysqli_num_rows($result) != 1) {
+	header("Location: $protocol://$server/index.php");
+	exit;
+}
+$assoc = mysqli_fetch_assoc($result);
+mysqli_free_result($result);
 if ($assoc['r_pwlvl'] > 0 && !isset($_SESSION['auth'])) {
 	/* unreadable for "normal" also unreadable for anonymous users */
 	header("Location: $protocol://$server/account/login.php");
@@ -31,14 +37,14 @@ $urlre = "((http{1}s?):\/\/)" . "((([[:alnum:]-])+(\.))+" . "([[:alnum:]]){2,6}"
 . "(:[0-9]{2,5})?)" . "(\/[[:alnum:]+=%#&_.~?@\-\/]*)";
 
 $body = nl2br(preg_replace("/$urlre/ium", '<a href="\\0">\\0</a>',
-	      str_replace("&amp;", "&", $assoc['body'])));
+	      str_replace("&amp;", "&", $assoc['body'])), false);
 
 $author = mysqli_fetch_assoc(mysqli_execute_query($dbc, "SELECT name, powerlevel FROM $table WHERE email='{$assoc['from_addr']}'"));
 ?>
 <?php
 $title = $assoc['subject'];
 include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
-if (!isset($_SESSION['auth']) || $assoc['r_pwlvl'] <= $_SESSION['powerlevel'])
+if ($assoc['r_pwlvl'] <= $_SESSION['powerlevel'])
 {
 ?>
 <h1><?=$assoc['subject']; ?></h1>
@@ -47,6 +53,11 @@ if (!isset($_SESSION['auth']) || $assoc['r_pwlvl'] <= $_SESSION['powerlevel'])
 "{$author['name']}</a> " .
 "&lt;<a href=\"mailto:{$assoc['from_addr']}\">{$assoc['from_addr']}</a>&gt; ";
 ?></p>
+<?php
+if ($assoc['relate_to'] != 0) {
+	echo "<p><a href=\"$protocol://$server/view.php?id={$assoc['relate_to']}\">Tin nhan truoc</a></p>";
+}
+?>
 <p>chinh sua lan cuoi: <?=$assoc['last_edit'];?></p>
 <?php
 if ($assoc['w_pwlvl'] <= $_SESSION['powerlevel']) {
@@ -57,7 +68,21 @@ echo "
 ?>
 <p><br><?=$body;?></p>
 <hr>
-<h3>Tra loi</h3>
+<?php
+$rmsg_result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable "
+	     . "WHERE relate_to=? AND r_pwlvl <= '{$_SESSION['powerlevel']}' "
+	     . "AND to_addr='{$assoc['to_addr']}'", [$id]);
+$rmsg_count = mysqli_num_rows($rmsg_result);
+echo "<h3>Tra loi ($rmsg_count)</h3>";
+if ($rmsg_count > 0) {
+	while ($rmsg = mysqli_fetch_assoc($rmsg_result)) {
+		echo "<h4><a href=\"$protocol://$server/view.php?" .
+		     "id={$rmsg['msg_id']}\">{$rmsg['subject']}</a></h4>";
+		echo "<p>Boi {$rmsg['from_addr']}<br></p>";
+		echo "<p>{$rmsg['body']}<br><br></p>";
+	}
+}
+?>
 <?php
 } else {
 	echo "<h3>Ban khong co quyen truy cap vao noi dung nay.</h3>";
