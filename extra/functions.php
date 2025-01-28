0ee92217cab1b4cd09025320f67676a65383bded
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-01-28 20:00:15 +0700

    add get_user_info function that return an assoc array of user info

--- extra/functions.php
+++ extra/functions.php
@@ -2,6 +2,12 @@
 /* See file COPYING for permissions and conditions to use the file. */
 ?>
 <?php
+function get_user_info($cond, $fields = "*", $arr) {
+	global $dbc, $table;
+	$query = "SELECT " . $fields . " FROM $table " . $cond;
+	$uinfo = mysqli_fetch_assoc(mysqli_execute_query($dbc, $query, $arr));
+	return $uinfo;
+}
 function secure_hash($password, $algo=PASSWORD_BCRYPT) {
 	switch ($algo) {
 	case PASSWORD_ARGON2ID:
