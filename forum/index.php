7f5cace899dadfb3aadb54ac1ad72cb84cc0f3e3
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-02-03 11:36:19 +0700

    print the author name, if exist in the user database

--- forum/index.php
+++ forum/index.php
@@ -18,9 +18,12 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>
 <?php
 echo "<h1>Cac bai viet</h1>\n";
-$result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable "
-	    . "WHERE relate_to=0 AND r_pwlvl<=? AND votes > -5 ORDER BY "
-	    . "votes DESC, created_at DESC", [$_SESSION['powerlevel']]);
+$result = mysqli_execute_query($dbc, "SELECT msg_id, subject, from_addr, "
+	    . "r_pwlvl, w_pwlvl, votes, created_at, name FROM $msgtable, "
+	    . "$table WHERE relate_to=0 AND r_pwlvl<=? AND votes > -5 "
+	    . "AND $msgtable.from_addr=$table.email "
+	    . "ORDER BY votes DESC, created_at DESC",
+	    [$_SESSION['powerlevel']]);
 if (!$result)
 	goto footer;
 while ($curmsg = mysqli_fetch_assoc($result)) {
@@ -33,8 +36,15 @@ while ($curmsg = mysqli_fetch_assoc($result)) {
 		[$cid, $_SESSION['powerlevel']]));
 	echo "<h3><a href=\"$protocol://$server/forum/index.php?id=".
 		"{$curmsg['msg_id']}\">{$curmsg['subject']}</a></h3>\n";
-	echo "<pre>{$curmsg['created_at']} tu {$curmsg['from_addr']}\n\n\n"
-	   . "<b>{$curmsg['votes']}</b>  $ncmt[0] nhan xet";
+	echo "<pre>{$curmsg['created_at']} tu ";
+	if ($curmsg['name']) {
+		echo "<a href=\"/profiles.php?email={$curmsg['from_addr']}\">"
+		   . "{$curmsg['name']}</a>\n\n\n";
+	} else {
+		echo "<a href=\"mailto:{$curmsg['from_addr']}\">"
+		   . "{$curmsg['from_addr']}</a>\n\n\n";
+	}
+	echo "<b>{$curmsg['votes']}</b>  $ncmt[0] nhan xet\n";
 	if ($curmsg['r_pwlvl'] > 0)
 		echo " (r={$curmsg['r_pwlvl']})";
 	if ($curmsg['w_pwlvl'] > 0)
