c1ffadbc566e81f50c4096e264834ccce46fb4e9
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-02-04 20:47:18 +0700

    print comment votes; change appearance to match duolingo forum

--- forum/view.php
+++ forum/view.php
@@ -42,40 +42,45 @@ if ($msginfo['relate_to'] != 0) {
 	   . "{$msginfo['relate_to']}\">(Tin nhan truoc)</a></p>\n";
 }
 ?>
-<p><?php echo "{$msginfo['votes']} | \n";
-if ($auth && $_SESSION['powerlevel'] >= 0) {
-$q = "SELECT amount FROM $votetable WHERE msg_id=? AND author=?";
-$myuid = $_SESSION['user_id'];
-$myvote = mysqli_fetch_row(mysqli_execute_query($dbc, $q, [$id, $myuid]));
-if (!isset($myvote[0]) || $myvote[0] == 0) {
-	echo "<a href=\"/forum/vote.php?id=$id&&amount=1\">+1 </a>\n";
-	echo "<a href=\"/forum/vote.php?id=$id&&amount=-1\">-1</a>\n";
-} else {
-	if ($myvote[0] == 1) {
-		echo "<a href=\"/forum/vote.php?id=$id&&amount=0\">+0 </a>\n";
-		echo "<a href=\"/forum/vote.php?id=$id&&amount=-1\">-1</a>\n";
-	} else {
-		echo "<a href=\"/forum/vote.php?id=$id&&amount=1\">+1 </a>\n";
-		echo "<a href=\"/forum/vote.php?id=$id&&amount=0\">-0</a>\n";
-	}
-}
-}
+<p><?php
 if (isset($msginfo['name']))
-	echo "Boi <a href=\"/profiles.php?email={$msginfo['from_addr']}\">"
+	echo "<a href=\"/profiles.php?email={$msginfo['from_addr']}\">"
 	   . "{$msginfo['name']}</a>\n";
 else
 	echo "<a href=\"mailto:{$msginfo['from_addr']}\">"
 	   . "{$msginfo['from_addr']}</a>\n";
 ?>
 </p>
+<p><?=$body;?>
+</p>
 <?php
-if ($msginfo['from_addr'] == $_SESSION['email'] || $_SESSION['powerlevel'] >= 50)
+if ($msginfo['from_addr'] == $_SESSION['email'] ||
+    $_SESSION['powerlevel'] >= 50) {
 	echo "<p><a href=\"/forum/board.php?editid={$msginfo['msg_id']}\">"
 	   . "Chinh sua</a></p>\n";
+}
+echo "<p><b>{$msginfo['votes']}</b> | \n";
+if ($auth && $_SESSION['powerlevel'] >= 0) { /* open brace 1 */
+$q = "SELECT amount FROM $votetable WHERE msg_id=? AND author=?";
+$myuid = $_SESSION['user_id'];
+$myvote = mysqli_fetch_row(mysqli_execute_query($dbc, $q, [$id, $myuid]));
+if (!isset($myvote[0]) || $myvote[0] == 0) {
+	echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
+	echo "<a href=\"/forum/vote.php?id=$id&amount=-1\">-1</a>\n";
+} else {
+	if ($myvote[0] == 1) {
+		echo "<a href=\"/forum/vote.php?id=$id&amount=0\">+0 </a>\n";
+		echo "<a href=\"/forum/vote.php?id=$id&amount=-1\">-1</a>\n";
+	} else {
+		echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
+		echo "<a href=\"/forum/vote.php?id=$id&amount=0\">-0</a>\n";
+	}
+}
+} /* end brace 1 */
 ?>
+</p>
 <pre><a href="<?="$protocol://$server{$_SERVER['REQUEST_URI']}";
 	   ?>"><?=$msginfo['created_at'];?></a></pre>
-<p><?=$body;?></p>
 <hr>
 <?php
 $rmsg_result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable, $table "
@@ -83,11 +88,11 @@ $rmsg_result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable, $table "
 	     . "AND to_addr='{$msginfo['to_addr']}' AND "
 	     . "$table.email=$msgtable.from_addr ORDER BY votes DESC", [$id]);
 $rmsg_count = mysqli_num_rows($rmsg_result);
-echo "<h3>$rmsg_count nhan xet</h3>\n";
 if ($auth && $msginfo['w_pwlvl'] <= $_SESSION['powerlevel']) {
 	echo "<p><a href=\"$protocol://$server/forum/board.php?relate_to=$id\">"
 	   . "Viet nhan xet</a></p>\n";
 }
+echo "<h3>$rmsg_count nhan xet</h3>\n";
 if ($rmsg_count > 0) {
 	while ($rmsg = mysqli_fetch_assoc($rmsg_result)) {
 		foreach ($rmsg as $k => $value) {
@@ -102,7 +107,8 @@ if ($rmsg_count > 0) {
 			   . "{$rmsg['from_addr']}</a>: ";
 		}
 		echo "{$rmsg['subject']}</h4>\n";
-		echo "<p>" . nl2br($rmsg['body'], false) . "</p>\n";
+		echo "<p>" . nl2br($rmsg['body'], false) . "\n</p>\n";
+		echo "<p><b>{$rmsg['votes']}</b></p>\n";
 		echo "<pre><a href=\"$protocol://$server/forum/index.php?"
 		   . "id={$rmsg['msg_id']}\">{$rmsg['created_at']}</a></pre>\n";
 	}
