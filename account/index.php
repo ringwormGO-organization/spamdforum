ccdfd1940835f17ced18554403b34970a8720f0c
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-02-10 22:05:23 +0700

    redirect to login if not logged in instead of printing random things

--- account/index.php
+++ account/index.php
@@ -1,21 +1,10 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
-?>
-<?php
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config.php");
-?>
-<?php
-	if ($_SESSION['auth']) {
-		header("Location: $protocol://$server/account/settings.php");
-		exit;
-	} else {
-		include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
-		echo "<p>Ban chua dang nhap.</p>";
-		echo "<p><a href=\"$protocol://$server/account/login.php\">Dang nhap tai khoan</a> ";
-		echo "<a href=\"$protocol://$server/account/register.php\">Dang ky tai khoan</a></p>";
-	}
-?>
-
-<?php
-include("{$_SERVER['DOCUMENT_ROOT']}/html/footer.html");
-?>
+if (isset($_SESSION['auth'])) {
+	header("Location: $protocol://$server/settings.php");
+	exit;
+} else {
+	header("Location: $protocol://$server/account/login.php");
+	exit;
+}
