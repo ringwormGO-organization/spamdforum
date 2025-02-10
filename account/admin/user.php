c978f442bd95f1f5355fbd38002a65ae2b14e0e1
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-02-10 22:02:26 +0700

    rename to words

--- account/admin/user.php
+++ account/admin/user.php
@@ -1,16 +1,13 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
-?>
-<?php
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config.php");
-require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/words.php");
 
 $msg = NULL;
 $ok = FALSE;
 if (isset($_POST['delete'])) {
 	$mypwlvl = $_SESSION['powerlevel'];
 	if ($mypwlvl < 100) {
-		$msg .= $usertablephp['msg']['err_nopriv'];
+		$msg .= $words['msg']['err_nopriv'];
 		goto stop;
 	}
 	if (!isset($_POST['delete_email'])) {
@@ -20,12 +17,12 @@ if (isset($_POST['delete'])) {
 		$query = "SELECT powerlevel FROM $table WHERE email=?";
 		$result = mysqli_execute_query($dbc, $query, [$email]);
 		if (mysqli_num_rows($result) < 1) {
-			$msg .= $usertablephp['msg']['err_not_found'];
+			$msg .= $words['msg']['err_not_found'];
 			goto stop;
 		}
 		$assoc = mysqli_fetch_assoc($result);
 		if ($assoc['powerlevel'] >= $mypwlvl) {
-			$msg .= $usertablephp['msg']['err_priv_unmet'];
+			$msg .= $words['msg']['err_priv_unmet'];
 			goto stop;
 		}
 		$delete_list[] = $email;
@@ -34,14 +31,14 @@ if (isset($_POST['delete'])) {
 		} else {
 			$target_num++;
 		}
-		$msg .= "{$usertablephp['msg']['added_to_list']} $email ({$assoc['powerlevel']}) \n";
+		$msg .= "{$words['msg']['added_to_list']} $email ({$assoc['powerlevel']}) \n";
 	}
 	if (isset($target_num)) {
 		$placeholder_email = "?" . str_repeat(",?", $target_num - 1);
 		$query = "DELETE FROM $table WHERE email IN ($placeholder_email) LIMIT $target_num";
 		$result = mysqli_execute_query($dbc, $query, $delete_list);
 		$delete_num = $target_num - mysqli_affected_rows($dbc);
-		$msg .= "{$usertablephp['msg']['delete_request']} $target_num, {$usertablephp['msg']['delete_failed']}: $delete_num";
+		$msg .= "{$words['msg']['delete_request']} $target_num, {$usertablephp['msg']['delete_failed']}: $delete_num";
 	}
 }
 
@@ -51,8 +48,8 @@ if (isset($_POST['delete'])) {
 	include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 ?>
 
-<h1><?=$usertablephp['h1_title']; ?></h1>    
-<p style="color:red;"><?=$usertablephp['p_red_notice']; ?></p>
+<h1><?=$words['h1_title']; ?></h1>
+<p style="color:red;"><?=$words['p_red_notice']; ?></p>
 
 <?php
 	if (isset($msg)) {
@@ -65,16 +62,16 @@ if (isset($_POST['delete'])) {
 	$num = mysqli_num_rows($result);
 
 	if ($num > 0) {
-		echo "<h3>{$usertablephp['user_num_msg'][0]} $num</h3>";
+		echo "<h3>{$words['user_num_msg'][0]} $num</h3>";
 		echo "<form action=\"{$_SERVER['PHP_SELF']}\" method=\"post\">\n";
 		echo "<table style=\"text-align:center; padding: 2px; \">
 <tr>
 	<th style=\"width: 0.2%; text-align: left;\">&nbsp;</th>
-	<th style=\"width: 1.5%; text-align: left;\">{$usertablephp['th_usertable']['id']}</th>
-	<th style=\"width: 3.4%; text-align: left;\">{$usertablephp['th_usertable']['name']}</th>
-	<th style=\"width: 4.6%; text-align: left;\">{$usertablephp['th_usertable']['email']}</th>
-	<th style=\"width: 2.2%; text-align: left;\">{$usertablephp['th_usertable']['reg_date']}</th>
-	<th style=\"width: 2.5%; text-align: left;\">{$usertablephp['th_usertable']['last_visit']}</th>
+	<th style=\"width: 1.5%; text-align: left;\">{$words['th_usertable']['id']}</th>
+	<th style=\"width: 3.4%; text-align: left;\">{$words['th_usertable']['name']}</th>
+	<th style=\"width: 4.6%; text-align: left;\">{$words['th_usertable']['email']}</th>
+	<th style=\"width: 2.2%; text-align: left;\">{$words['th_usertable']['reg_date']}</th>
+	<th style=\"width: 2.5%; text-align: left;\">{$words['th_usertable']['last_visit']}</th>
 </tr>
 \n";
 		while ($row = mysqli_fetch_row($result)) {
@@ -93,10 +90,10 @@ if (isset($_POST['delete'])) {
 ";
 		}
 		echo '</table>';
-		echo "<div style=\"text-align: center;\"><input type=\"submit\" name=\"delete\" value=\"{$usertablephp['input']['delete']}\"></div>";
+		echo "<div style=\"text-align: center;\"><input type=\"submit\" name=\"delete\" value=\"{$words['input']['delete']}\"></div>";
 		echo '</form>';
 	} else {
-		echo "<p>{$usertablephp['user_num_msg'][1]}</p>";
+		echo "<p>{$words['user_num_msg'][1]}</p>";
 	}
 ?>
 
