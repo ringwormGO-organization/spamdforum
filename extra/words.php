a1ab839fed671ad0bf3e393d59fe38fbc6cc1dec
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-02-10 22:06:28 +0700

    automatically get words for all scripts

--- extra/words.php
+++ extra/words.php
@@ -1,7 +1,18 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
-?>
-<?php
-require("{$_SERVER['DOCUMENT_ROOT']}/extra/words/words_vi.php");
-//require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/words/words_en.php");
+if (!isset($_COOKIE['spamdforum_lang'])) {
+	setcookie('spamdforum_lang', 'vi', time()+2592000, '/', '', $secure,
+		    false);
+	$f_lang = 'vi';
+} else {
+	$f_lang = $_COOKIE['spamdforum_lang'];
+}
+$common_fn = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang/common.json";
+$fn = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang{$_SERVER['SCRIPT_NAME']}.json";
+$words = json_decode(file_get_contents($fn), true);
+$common = json_decode(file_get_contents($common_fn), true);
+if ($_SERVER['SCRIPT_NAME'] == '/forum/index.php') {
+	$view = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang/forum/view.php.json";
+	$words = array_merge($words, json_decode(file_get_contents($view), true));
+}
 ?>
