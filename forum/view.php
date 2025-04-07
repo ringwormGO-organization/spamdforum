4aa8d45b6bc7e5ab3a64bfdedba8b27a010d9cc7
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-04-07 13:42:17 +0700

    update url regex to match ip address; list all comments with relate_to=msg_id

--- forum/view.php
+++ forum/view.php
@@ -22,7 +22,7 @@ foreach ($msginfo as $k => $value) {
  * \\9 for path
  * \\2 for the protocol, \\1 for protocol with ://
  */
-$urlre = "((http{1}s?):\/\/)" . "((([[:alnum:]-])+(\.))+" . "([[:alnum:]]){2,6}"
+$urlre = "((http{1}s?):\/\/)" . "((([[:alnum:]-])+(\.))+" . "([[:alnum:]]){1,6}"
 . "(:[0-9]{2,5})?)" . "(\/[[:alnum:]+=%#&_.:~?@\-\/]*)?";
 
 $body = nl2br(preg_replace("/$urlre/ium", '<a href="\\0">\\0</a>',
@@ -60,23 +60,23 @@ if ($msginfo['from_addr'] == $_SESSION['email'] ||
 	   . "{$words['edit']}</a></p>\n";
 }
 echo "<p><b>{$msginfo['votes']}</b> | \n";
-if ($auth && $_SESSION['powerlevel'] >= 0) { /* open brace 1 */
-$q = "SELECT amount FROM $votetable WHERE msg_id=? AND author=?";
-$myuid = $_SESSION['user_id'];
-$myvote = mysqli_fetch_row(mysqli_execute_query($dbc, $q, [$id, $myuid]));
-if (!isset($myvote[0]) || $myvote[0] == 0) {
-	echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
-	echo "<a href=\"/forum/vote.php?id=$id&amount=-1\">-1</a>\n";
-} else {
-	if ($myvote[0] == 1) {
-		echo "<a href=\"/forum/vote.php?id=$id&amount=0\">+0 </a>\n";
+if ($auth && $_SESSION['powerlevel'] >= 0) {
+	$q = "SELECT amount FROM $votetable WHERE msg_id=? AND author=?";
+	$myuid = $_SESSION['user_id'];
+	$myvote = mysqli_fetch_row(mysqli_execute_query($dbc, $q, [$id, $myuid]));
+	if (!isset($myvote[0]) || $myvote[0] == 0) {
+		echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
 		echo "<a href=\"/forum/vote.php?id=$id&amount=-1\">-1</a>\n";
 	} else {
-		echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
-		echo "<a href=\"/forum/vote.php?id=$id&amount=0\">-0</a>\n";
+		if ($myvote[0] == 1) {
+			echo "<a href=\"/forum/vote.php?id=$id&amount=0\">+0 </a>\n";
+			echo "<a href=\"/forum/vote.php?id=$id&amount=-1\">-1</a>\n";
+		} else {
+			echo "<a href=\"/forum/vote.php?id=$id&amount=1\">+1 </a>\n";
+			echo "<a href=\"/forum/vote.php?id=$id&amount=0\">-0</a>\n";
+		}
 	}
 }
-} /* end brace 1 */
 ?>
 </p>
 <pre><a href="<?="$protocol://$server{$_SERVER['REQUEST_URI']}";
@@ -85,8 +85,7 @@ if (!isset($myvote[0]) || $myvote[0] == 0) {
 <?php
 $rmsg_result = mysqli_execute_query($dbc, "SELECT * FROM $msgtable, $table "
 	     . "WHERE relate_to=? AND r_pwlvl <= '{$_SESSION['powerlevel']}' "
-	     . "AND to_addr='{$msginfo['to_addr']}' AND "
-	     . "$table.email=$msgtable.from_addr ORDER BY votes DESC", [$id]);
+	     . "AND $table.email=$msgtable.from_addr ORDER BY votes DESC", [$id]);
 $rmsg_count = mysqli_num_rows($rmsg_result);
 if ($auth && $msginfo['w_pwlvl'] <= $_SESSION['powerlevel']) {
 	echo "<p><a href=\"$protocol://$server/forum/board.php?relate_to=$id\">"
