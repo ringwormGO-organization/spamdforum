4992ee8350da3cdeed449facf8573a11e35bba19
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-04-08 18:52:04 +0700

    add a function that use left join to query efficiently

--- forum/vote.php
+++ forum/vote.php
@@ -1,6 +1,15 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config.php");
+function get_msg_votes($msg_id, $uid) {
+	global $dbc, $msgtable, $votetable;
+	$query = "SELECT r_pwlvl, votes, amount FROM $msgtable LEFT JOIN "
+		. "$votetable ON $votetable.msg_id=? AND forum_votes.author=? "
+		. "WHERE forum_msg.msg_id=?";
+	$arr = [$msg_id, $uid, $msg_id];
+	$msginfo = mysqli_fetch_assoc(mysqli_execute_query($dbc, $query, $arr));
+	return $msginfo;
+}
 $id = 0;
 if (!empty($_GET['id']) && isset($_GET['amount'])) {
 	$id = intval($_GET['id']);
@@ -11,16 +20,12 @@ if (!empty($_GET['id']) && isset($_GET['amount'])) {
 		goto end;
 	if (!$auth || $_SESSION['powerlevel'] < 0)
 		goto end;
-	$target = get_msg_info("WHERE msg_id=?", "r_pwlvl, votes", [$id]);
+	$myuid = $_SESSION['user_id'];
+	$target = get_msg_votes($id, $myuid);
 	if ($target['r_pwlvl'] > $_SESSION['powerlevel'])
 		goto end;
-	$myuid = $_SESSION['user_id'];
-	$myvoteq = "SELECT amount FROM $votetable WHERE msg_id=? AND "
-		 . "author=?";
-	$myvote = mysqli_fetch_row(mysqli_execute_query($dbc, $myvoteq,
-				   [$id, $myuid]));
-	if (isset($myvote[0]))
-		$cur_amount = intval($myvote[0]);
+	if (isset($target['amount']))
+		$cur_amount = intval($target['amount']);
 	else
 		$cur_amount = 0;
 	$change = $amount - $cur_amount;
@@ -32,7 +37,7 @@ if (!empty($_GET['id']) && isset($_GET['amount'])) {
 		   . "VALUES (?, ?, ?)";
 	$update_query = "UPDATE $votetable SET amount=? WHERE msg_id=?";
 	$update_msg = "UPDATE $msgtable SET votes=? WHERE msg_id=?";
-	if (!isset($myvote[0]))
+	if ($target['amount'] === NULL)
 		mysqli_execute_query($dbc, $ins_query, [$id, $myuid, $amount]);
 	else
 		mysqli_execute_query($dbc, $update_query, [$amount, $id]);
