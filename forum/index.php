65aca65e0d564e52a385e80d63b40ad09c75ba39
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-05-10 15:43:12 +0700

    add a form to sort messages by newest and most voted, in the future there will be a drop down list for all topics

--- forum/index.php
+++ forum/index.php
@@ -20,6 +20,13 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html");
 <?php
 $start = hrtime(true);
 echo "<h1>{$words['msg_list']}</h1>\n";
+?>
+<form name="adjust_content" action="<?=$_SERVER['PHP_SELF'];?>">
+	<p><input type="radio" name="sortby" value="new"> <?=$words['sortby_new'];?>
+	<input type="radio" name="sortby" value="top"> <?=$words['sortby_top'];?>
+	<input type="submit" value="<?=$words['adjust_content'];?>"></p>
+</form>
+<?php
 $orderby = "created_at DESC, votes DESC";
 if (!empty($_GET['sortby'])) {
 	if ($_GET['sortby'] == 'new')
