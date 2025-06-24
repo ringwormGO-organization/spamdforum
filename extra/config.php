d09e86b6b1214e7e750a8e810b8b4491b1ca1828
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-06-24 10:00:55 +0700

    move dbconnect.php to extra/

--- extra/config.php
+++ extra/config.php
@@ -12,7 +12,9 @@ if ($protocol == 'https' || isset($_SERVER['HTTPS'])) {
 }
 session_set_cookie_params(['lifetime' => time() + 2592000, 'path' => '/', 'domain' => '', 'secure' => $secure, 'httponly' => true]);
 session_start();
-require_once("{$_SERVER['DOCUMENT_ROOT']}/../dbconnect.php");
+$need_db = true;
+if($need_db)
+	require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/dbconnect.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/variables.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/functions.php");
 require_once("{$_SERVER['DOCUMENT_ROOT']}/extra/config_access.php");
