7f74493aef7abe1a6c8bb4bda202c8e3e36dac7c
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-06-26 22:07:05 +0700

    merge with English words to display English when a translation is not available

--- extra/words.php
+++ extra/words.php
@@ -8,7 +8,10 @@ if (!isset($_COOKIE['spamdforum_lang'])) {
 }
 $common_fn = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang/common.json";
 $fn = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang{$_SERVER['SCRIPT_NAME']}.json";
+$eng = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/en{$_SERVER['SCRIPT_NAME']}.json";
 $words = json_decode(@file_get_contents($fn), true);
+/* merge with English words to display English when a translation is not available */
+$words = array_merge(json_decode(@file_get_contents($eng), true), $words);
 $common = json_decode(file_get_contents($common_fn), true);
 if ($_SERVER['SCRIPT_NAME'] == '/forum/index.php') {
 	$view = "{$_SERVER['DOCUMENT_ROOT']}/extra/words/$f_lang/forum/view.php.json";
