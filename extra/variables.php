1a3b3486db90ad9738619b2f829b7de9d46ba50f
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-06-30 17:33:25 +0700

    delete superflous lines

--- extra/variables.php
+++ extra/variables.php
@@ -1,7 +1,5 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
-?>
-<?php
 $_SERVER['PHP_SELF'] = htmlspecialchars($_SERVER['PHP_SELF']);
 if (isset($_SERVER['PATH_INFO'])) {
 	$_SERVER['PATH_INFO'] = htmlspecialchars($_SERVER['PATH_INFO']);
