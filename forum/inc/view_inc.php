b29b0cf7ac3233ba4bf5a4d0ea149bfe98c41fcf
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-07-02 17:26:47 +0700

    add a subset of markdown (link, image, hr) support; print <br>\n when there's one newline, </p>\n<p> when there's two newline

---
+++ forum/inc/view_inc.php
@@ -0,0 +1,29 @@
+<?php
+function format_body($body) {
+	/*
+	 * Groups are separated extensively so admins can config how they want
+	 * links to displays.
+	 * \\0 full url.
+	 * \\3 for hostname
+	 * \\4 for the domain name with a . but without com, net, etc
+	 * \\7 for com, net, etc
+	 * \\9 for the path (/new, /file/abc.jpg, etc, may be empty)
+	 * \\2 for the protocol
+	 * \\8 for the port with a : (may be empty)
+	 */
+	$urlre = "(^| )(https?):\/\/" . "((([[:alnum:]-])+(\.))+" . "([[:alnum:]]{1,6})"
+	. "(:[0-9]{2,5})?)" . "(\/[[:alnum:]+=%#&_.:~?@\-\/]*)?( |$)";
+	
+	$imgurl = "(^| )\!\[(.*)\]\((.+)\)( |$)";
+	$markurl = "(^| )\[(.+)\]\((.+)\)( |$)";
+	
+	$body = preg_replace("/$urlre/ium", '<a href="\\0">\\0</a>', $body);
+	$body = preg_replace("/$imgurl/ium", '<img alt="\\2" src="\\3">', $body);
+	$body = preg_replace("/$markurl/ium", '\\1<a href="\\3">\\2</a>\\4', $body);
+	$body = preg_replace("/^ *&gt;(.+)$/ium", "<blockquote><p>\\1</p></blockquote>", $body);
+	$body = preg_replace("/^ *-{3,}$/ium", "<hr>", $body);
+	$body = nl2br($body, false);
+	$body = preg_replace("/<br>\n *<br>\n/ium", "</p>\n<p>", $body);
+	return $body;
+}
+?>
