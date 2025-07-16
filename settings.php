1da2396a806a82a79333b4e3de8b513c7c88fc15
hahahahacker2009 <hahahahacker2009@gmail.com>
2025-07-16 11:50:02 +0700

    rework on language naming, so we can print all languages and its name in a loop

--- settings.php
+++ settings.php
@@ -1,11 +1,13 @@
 <?php
 /* See file COPYING for permissions and conditions to use the file. */
 ob_start();
+$langlist = array('en' => 'English (Tiếng Anh)',
+		  'vi' => 'Tiếng Việt (Vietnamese)'
+		);
 if (!isset($_POST['submit']))
 	goto html;
 if (!empty($_POST['lang'])) {
-	$langlist = ['en', 'vi'];
-	if (in_array($_POST['lang'], $langlist)) {
+	if (array_key_exists($_POST['lang'], $langlist)) {
 		setcookie('spamdforum_lang', $_POST['lang'], time()+2592000,
 			  '/', '', false, false);
 		$_COOKIE['spamdforum_lang'] = $_POST['lang'];
@@ -26,9 +28,12 @@ include("{$_SERVER['DOCUMENT_ROOT']}/html/header.html")
 <p><a href="/account/info.php"><?=$words['link_account'];?></a></p>
 <form name="lang" action="<?=$_SERVER['PHP_SELF'];?>" method="POST">
 <p><?=$words['languages'];?>: <select name="lang">
-<option value=""><?=$f_lang;?></option>
-<option value="en">English (Tiếng Anh)</option>
-<option value="vi">Tiếng Việt (Vietnamese)</option>
+<?php
+echo "<option value=\"$f_lang\">{$langlist[$f_lang]}</option>\n";
+unset($langlist[$f_lang]);
+foreach ($langlist as $lang => $name)
+	echo "<option value=\"$lang\">$name</option>\n";
+?>
 </select></p>
 <p><input type="submit" name="submit" value="ok"></p>
 </form>
